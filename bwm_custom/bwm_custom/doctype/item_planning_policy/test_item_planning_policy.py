b2ddf1db9d38ef3b55137e53836b9553aa2b184d
Aruna-1108 <aruna@DESKTOP-4H58JKD.localdomain>
2025-11-10 11:35:13 +0530

    Item planning Policy

---
+++ bwm_custom/bwm_custom/doctype/item_planning_policy/test_item_planning_policy.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Aruna Devi and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestItemPlanningPolicy(FrappeTestCase):
+	pass
